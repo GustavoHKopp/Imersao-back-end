8e3afcd754cb13b5732e6c5c72e3f3c3298571e3
gustavoKopp <gustavo.kopp@sisplansistemas.com.br>
2024-11-19 08:54:12 -0300

    Create mock posts and created get routes

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: 'Uma foto',
+        imagem: 'https://placecats.com/millie/300/150',
+    },
+    {
+        id: 2,
+        descricao: 'Um gatinho curioso',
+        imagem: 'https://placecats.com/200/300',
+    },
+    {
+        id: 3,
+        descricao: 'Gato dormindo',
+        imagem: 'https://placecats.com/millie/300/150',
+    }
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Servidor está rodando...');
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send('Teste ABC');
+function buscaPostPorId(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts)
+});
+
+app.get('/posts/:id', (req, res) => {
+    const index = buscaPostPorId(req.params.id)
+    res.status(200).json(posts[index])
 });
